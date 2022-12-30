11f8256ed404bbcd4e3fefdef83c46a375fcf51e
cornusandu <34373732+cornusandu@users.noreply.github.com>
2022-12-30 12:28:55 +0200

    Create Infinity-Posibility.py

---
+++ Infinity-Posibility.py
@@ -0,0 +1,11 @@
+import time
+def wait(timer, repeats=0):
+  for i in range(repeats):
+      time.sleep(timer)
+def start_turtle():
+  import turtle
+  turtle.color('black')
+def end_script():
+  print('_______________________')
+  print('Made with "Inifnity-Posbility" module.')
+  exit()
