b88a45fa9f505a4a31d61d60fb0f9983f72a539b
cornusandu <34373732+cornusandu@users.noreply.github.com>
2022-12-30 16:31:49 +0200

    Update Infinity-Posibility.py

--- Infinity-Posibility.py
+++ Infinity-Posibility.py
@@ -1,17 +1,25 @@
 import time
 from turtle import *
-def wait(timer, repeats=0):
+import random
+def wait(timer=1, repeats=1):
   for i in range(repeats):
       time.sleep(timer)
 def end_script():
   print('_______________________')
-  print('Made with "Ifnity-Posbility" module.')
+  print('Made with "Infinity-Posbility" module.')
   exit()
-def draw_squares(times=10, size=30):
+def draw_squares(NoPattern=True,times=10, size=30):
   for i in range(times):
         for i in range(4):
             forward(30)
             right(90)
-        forward(32)
-        right(45)
-        forward(18)
+        if Pattern:
+          forward(32)
+          right(45)
+          forward(18)
+        else:
+          forward(random.randint(20, 45))
+          right(random.randint(2, 182))
+          forward(random.randint(4, 25))
+def get_random_number(min=0, max=10):
+    return random.randint(min,max)
